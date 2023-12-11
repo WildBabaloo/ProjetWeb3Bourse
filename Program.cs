f4194b28967e767a92cbc0f9a358be0838c988f2
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-12-11 12:39:31 -0500

    Added API Javascript Fetch for IndexBourse

--- Program.cs
+++ Program.cs
@@ -45,6 +45,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapHub<BourseHub>("/BourseHub");
+app.MapHub<BourseHub>("/bourseHub");
 
 app.Run();
---
+++ wwwroot/js/IndexBourse.js
@@ -0,0 +1,31 @@
+const url = "/api/BourseAPI"
+let $bourses = $("#bourses")
+getBourses();
+
+function getBourses() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => data.forEach(bourse => {
+            let template = `<tr>
+                                <td>${bourse.nom}</td>
+                                <td>${bourse.valeur}</td>
+                                <td>${bourse.variation}</td>
+                                <td>
+                                <a href = "/Bourses/Edit/${bourse.id}">Edit</a> |
+                                <a href = "/Bourses/Delete/${bourse.id}">Delete</a>
+                                </td>
+                             </tr>`;
+            $bourses.append($(template));
+        }
+        ))
+        .catch(error => alert("Erreur API"));
+}
+
+const connection = new signalR.HubConnectionBuilder().withUrl("/bourseHub").build();
+connection.start()
+    .catch(function (err) { return console.error(err.tostring()) })
+
+connection.on("BourseChange", function () {
+    $bourses.empty();
+    getBourses();
+})
