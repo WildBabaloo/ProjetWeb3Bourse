c7ebcf7016928d8bc719ef901954f11fcd991bd3
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-12-12 13:09:21 -0500

    Updated event model

--- Models/Evenement.cs
+++ Models/Evenement.cs
@@ -1,9 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjetWeb3Bourse.Models {
     public class Evenement {
 
         public int id { get; set; }
+
+        [ForeignKey("Bourse")]
+        public int bourseId { get; set; }
+        public Bourse? bourse { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime date { get; set; }
         [DataType(DataType.Date)]
