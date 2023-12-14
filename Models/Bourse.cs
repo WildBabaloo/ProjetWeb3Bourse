b1095f8c4605bfd7de244d2ee16919b4f5594efb
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-12-14 00:43:22 -0500

    Changed display names + some level of polish no css fuck css. All my homies hate css

--- Models/Bourse.cs
+++ Models/Bourse.cs
@@ -1,13 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace ProjetWeb3Bourse.Models {
     public class Bourse {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
+        [DisplayName("Nom")]
         public string nom { get; set; }
+        [DisplayName("Valeur")]
         public double valeur { get; set; }
+        [DisplayName("Variation")]
         public double variation { get; set; }
         public ICollection<Evenement>? Evenements { get; set; }
 
--- Models/Evenement.cs
+++ Models/Evenement.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjetWeb3Bourse.Models {
@@ -12,10 +13,14 @@ namespace ProjetWeb3Bourse.Models {
         public Bourse? bourse { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayName("Date")]
         public DateTime date { get; set; }
         [DataType(DataType.Time)]
+        [DisplayName("Heure")]
         public DateTime heure { get; set; }
+        [DisplayName("Valeur")]
         public double valeur { get; set; }
+        [DisplayName("Variation")]
         public double variation { get; set; }
 
     }
